1be904b864e9f56cf8280ef9b78140bdd7452b1d
CaRoxas <carlotacarballolol@gmail.com>
2024-05-20 19:40:01 +0100

    Día 20/05 PT.2
    añadir más árboles, intentar animación de puerta y el agua colocar ole ole

---
+++ Assets/Script/Casita.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casita : MonoBehaviour
+{
+    Animator animatione;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Diva")
+        {
+            animatione.SetBool("Abril",true);
+        }
+    }
+}
